ffc7d5761d5c7d0a29d9cdb3b7622ccb25aecf7a
cg-devato <chui@devatotech.com>
2014-09-24 22:02:45 +0200

    modify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -71,11 +71,12 @@ class HomeController < ApplicationController
         Device.create_by_device_id(dev_id, user)
         user = sign_in( :user, user )
         user_info = {id:user.id.to_s, username:user.name,email:user.email,token:user.authentication_token,avatar:user.avatar_url}
+        setting   = {push_enable:user.push_enable,sound_enable:user.sound_enable,auto_accept_friend:user.auto_accept_friend,auto_notify_friend:user.auto_notify_friend, theme_type:user.theme_type}
         if social_type == User::SOCIAL_TYPES[0]
           user.send_notification_to_all_users
           UserMailer.welcome(user).deliver
         end
-        render :json => {data:user_info,message:{type:'success',value:'Signed up successfully', code: TimeChatNet::Application::SUCCESS_LOGIN}}
+        render :json => {data:{user_info:user_info,setting:setting},message:{type:'success',value:'Signed up successfully', code: TimeChatNet::Application::SUCCESS_LOGIN}}
       end
     end     
   end
@@ -100,9 +101,9 @@ class HomeController < ApplicationController
         Device.create_by_device_id(dev_id,resource)
         user = sign_in(:user, resource)
         resource.update_attributes(time_zone:timezone)
-        user_info={id:resource.id.to_s, username:resource.name,email:resource.email,token:resource.authentication_token,avatar:resource.avatar_url}
-        
-        render :json => {data:user_info,message:{type:'success',value:'login success', code: TimeChatNet::Application::SUCCESS_LOGIN}}
+        user_info ={id:resource.id.to_s, username:resource.name,email:resource.email,token:resource.authentication_token,avatar:resource.avatar_url}
+        setting   = {push_enable:user.push_enable,sound_enable:user.sound_enable,auto_accept_friend:user.auto_accept_friend,auto_notify_friend:user.auto_notify_friend, theme_type:user.theme_type}
+        render :json => {data:{user_info:user_info, setting:setting},message:{type:'success',value:'login success', code: TimeChatNet::Application::SUCCESS_LOGIN}}
       else
         render :json => {data:[],message:{type:'error',value:'Password is incorrect', code: TimeChatNet::Application::ERROR_LOGIN}}
       end
