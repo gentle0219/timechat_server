2058680a8c4694b740d1d435ee00c8d0ca91f638
cg-devato <chui@devatotech.com>
2014-10-14 18:57:26 +0200

    modify

--- app/models/user.rb
+++ app/models/user.rb
@@ -342,7 +342,7 @@ class User
     devices.each do |device|
       if Rails.env.production?
         if device.platform == Device::DEVICE_PLATFORM[0]    # in case platform is ios
-          APNS.send_notification(device.dev_id,alert:message, badge:count, sound: 'default') if device.dev_id.present?
+          APNS.send_notification(device.dev_id,alert:message, badge:count, sound: 'sms_alert_popcorn') if device.dev_id.present?
         else
           destination = [device.dev_id]
           data = {:alert=>notification.message}
@@ -358,7 +358,7 @@ class User
     devices.each do |device|
       if Rails.env.production?
         if device.platform == Device::DEVICE_PLATFORM[0]    # in case platform is ios
-          APNS.send_notification(device.dev_id,alert:message, badge:device.badge_count+1, sound: 'default') if device.dev_id.present?
+          APNS.send_notification(device.dev_id,alert:message, badge:device.badge_count+1, sound: 'sms_alert_popcorn') if device.dev_id.present?
         else
           destination = [device.dev_id]
           data = {:alert=>notification.message}
